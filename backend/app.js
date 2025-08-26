85697f70cb72b0f50885a19b6f6992fea30247f9
KM Rejoan Tanjim <kmrejoantanjim@gmail.com>
2025-08-27 00:18:11 +0600

    staring backend development for jobHunt

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send("jobHunt server is cooking.....");
+});
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOption = {
+  origin: "http//localhost:5173",
+  Credentials: true,
+};
+app.use(cors(corsOption));
+
+export default app;
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config();
+import app from "./app.js";
+// import connectDB from "./config/db.js";
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  return res.send("jobHunt server is cooking.....");
+});
+
+// connect db
+// connectDB();
+
+// server start
+app.listen(PORT, () => {
+  console.log(`server is running at port ${PORT}`);
+});
